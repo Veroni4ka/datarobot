4c97a914e0bfdacb4a4f0fd5b3fd52a52dfdeb55
Veroni4ka <veronika.kolesnikova@yahoo.com>
2015-04-21 21:08:08 -0400

    Google Prediction API and Python solution

---
+++ prediction1.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+#
+
+"""
+To run the program type:
+  $ python prediction.py object_name="bucket/object" id="model_id" project="project_id"
+"""
+
+__author__ = ('veronika.kolesnikova@yahoo.com (Veronika Kolesnikova)')
+
+import argparse
+import os
+import pprint
+import sys
+import time
+
+from apiclient import discovery
+from apiclient import sample_tools
+from oauth2client import client
+
+
+# Time to wait (in seconds) between successive checks of training status.
+SLEEP_TIME = 10
+
+
+
+# Declare command-line flags.
+argparser = argparse.ArgumentParser(add_help=False)
+argparser.add_argument('object_name',
+                     help='Full Google Storage path of csv data (ex bucket/object)')
+argparser.add_argument('id',
+                     help='Model Id of your choosing to name trained model')
+argparser.add_argument('project',
+                     help='Project ID from Google Developers Console')
+
+
+def print_header(line):
+  '''Format and print header block sized to length of line'''
+  header_str = '='
+  header_line = header_str * len(line)
+  print '\n' + header_line
+  print line
+  print header_line
+
+
+def main(argv):
+  
+  service, flags = sample_tools.init(
+      argv, 'prediction', 'v1.6', __doc__, __file__, parents=[argparser],
+      scope=('https://www.googleapis.com/auth/prediction', 'https://www.googleapis.com/auth/devstorage.full_control'))
+  pid="spam"
+  object_name="dr_hometask/english.csv"
+  project="crested-surfer-91819"
+  try:
+    # Get access to the Prediction API.
+    papi = service.trainedmodels()
+
+    # List models.
+    print_header('Fetching list of models')
+    result = papi.list(project=project).execute()
+    print 'List results:'
+    pprint.pprint(result)
+
+ # Start training request on a data set.
+    print_header('Submitting model training request')
+    body = {'id':pid, 'storageDataLocation':object_name}
+    start = papi.insert(project=project, body=body).execute()
+    print 'Training results:'
+    pprint.pprint(start)
+
+    # Wait for the training to complete.
+    print_header('Waiting for training to complete')
+    while True:
+      status = papi.get(project=project, id=pid).execute()
+      state = status['trainingStatus']
+      print 'Training state: ' + state
+      if state == 'DONE':
+        break
+      elif state == 'RUNNING':
+        time.sleep(SLEEP_TIME)
+        continue
+      else:
+        raise Exception('Training Error: ' + state)
+
+      # Job has completed.
+      print 'Training completed:'
+      pprint.pprint(status)
+      break
+
+   #Analyze
+    result = papi.analyze(id=pid, project=project).execute()
+    print 'Analyze results:'
+    pprint.pprint(result)
+
+    # Make a prediction using the newly trained model.
+    print_header('Making a prediction')
+    body = {'input': {'csvInstance': ["Hey buy some stuff"]}}
+    result = papi.predict(body=body, id=pid, project=project).execute()
+    print 'Prediction results...'
+    pprint.pprint(result)
+
+    # Delete model.
+    #print_header('Deleting model')
+    #result = papi.delete(id=flag.id, project=flag.project).execute()
+    #print 'Model deleted.'
+
+  except client.AccessTokenRefreshError:
+    print ("The credentials have been revoked or expired, please re-run"
+      "the application to re-authorize")
+
+
+if __name__ == '__main__':
+  main(sys.argv)
